452f35e623f37f28b5a8dba38ac1c99695510a62
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-15 22:03:05 +0545

    done

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:3500"; 
+const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:5173"; 
 
 // unauthenticated user register/login
 const API = axios.create({
