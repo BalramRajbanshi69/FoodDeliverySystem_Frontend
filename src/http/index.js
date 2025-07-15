ed0eeaee81049a001a94c68caec935ab529e7a1e
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-15 21:12:56 +0545

    vercel

--- src/http/index.js
+++ src/http/index.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:3500"; 
 
 // unauthenticated user register/login
 const API = axios.create({
-    baseURL : "http://localhost:3500/api",
+    baseURL : API_BASE_URL + "/api",
     headers:{
         "Content-Type":"application/json",            // if form,image use multipart/formData
         "Accept":"application/json"
@@ -12,7 +13,7 @@ const API = axios.create({
 
 // authenticated user (giving permision)
 const APIAuthenticated = axios.create({
-    baseURL : "http://localhost:3500/api",
+    baseURL : API_BASE_URL + "/api",
     headers:{
         "Content-Type":"application/json",            // if form,image use multipart/formData
         "Accept":"application/json",
