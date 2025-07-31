5948cbdcc61d2e0fd2d0041770a66d714794e730
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-31 15:02:31 +0545

    done

--- src/http/index.js
+++ src/http/index.js
@@ -1,25 +1,74 @@
+// import axios from "axios";
+
+// const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:3500"; 
+
+// // unauthenticated user register/login
+// const API = axios.create({
+//     baseURL : API_BASE_URL + "/api",
+//     headers:{
+//         "Content-Type":"application/json",            // if form,image use multipart/formData
+//         "Accept":"application/json"
+//     }
+// })
+
+// // authenticated user (giving permision)
+// const APIAuthenticated = axios.create({
+//     baseURL : API_BASE_URL + "/api",
+//     headers:{
+//         "Content-Type":"application/json",            // if form,image use multipart/formData
+//         "Accept":"application/json",
+//         "Authorization":`${localStorage.getItem("token")}`
+//     }
+// })
+
+
+// export {API,APIAuthenticated}
+
+
+
+
+
+
+
+
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:3500"; 
+const API_BASE_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:3500";
 
-// unauthenticated user register/login
+// Unauthenticated API instance
 const API = axios.create({
-    baseURL : API_BASE_URL + "/api",
-    headers:{
-        "Content-Type":"application/json",            // if form,image use multipart/formData
-        "Accept":"application/json"
-    }
-})
+  baseURL: API_BASE_URL + "/api",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+});
 
-// authenticated user (giving permision)
+// Authenticated API instance
 const APIAuthenticated = axios.create({
-    baseURL : API_BASE_URL + "/api",
-    headers:{
-        "Content-Type":"application/json",            // if form,image use multipart/formData
-        "Accept":"application/json",
-        "Authorization":`${localStorage.getItem("token")}`
+  baseURL: API_BASE_URL + "/api",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+});
+
+// Add Bearer token dynamically using request interceptor
+APIAuthenticated.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-})
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { API, APIAuthenticated };
 
 
-export {API,APIAuthenticated}
+
+
